19e83e599f2581d6eb4392aa7e93f857c0f0a7d8
Iliyana Vitanova <iliyanavitanova96@gmail.com>
2018-10-30 14:00:07 +0200

    changes for app/server config

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 const config = require('./database/DB');
 const ProductRouter = require('./routes/ProductRoutes');
+const UserRouter = require('./routes/UserRoutes');
 
 mongoose.connect(config.DB).then(
     () => {console.log('Database is connected') },
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/product', ProductRouter);
+app.use('/user', UserRouter);
 
 app.listen(PORT, function(){
   console.log('Server is running on Port: ',PORT);
